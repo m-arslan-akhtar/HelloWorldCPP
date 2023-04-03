47cd3287609f36ac208698224ef05af37658a43c
aman <arslan@digidiv.pk>
2023-04-03 16:35:34 +0500

    1st Initial commit

---
+++ app/src/main/java/com/example/helloworldcpp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.helloworldcpp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.example.helloworldcpp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'helloworldcpp' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'helloworldcpp' library on application startup.
+        init {
+            System.loadLibrary("helloworldcpp")
+        }
+    }
+}
